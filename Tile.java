8da6f54c541d3dfe12080755638a6a81c42d9534
RAWhitf18 <ryanwhitfield27@gmail.com>
2015-06-15 14:28:04 -0400

    Tile.java

---
+++ Tile.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+
+public class Tile
+{
+    private String iD;
+    private double x;
+    private double y;
+    private double r;
+    
+    
+    public Tile()
+    {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    
+    public void drawFilled()
+    {
+        double[] xFill = { };
+        double[] yFill = { };
+        
+        StdDraw.setPenColor();
+        StdDraw.filledSquare();    
+    }
+    
+    
+    public void contains()
+    {
+        
+        
+    }
+    
+    public static void main(String[], args)
+    {
+        StdDraw.swr
+        
+        
+        
+        
+    }
+    
+}
